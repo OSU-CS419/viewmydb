22e2a36beef5ec5f68dcffef32fb4ec4feea9699
davidtadams <adams.t.david@gmail.com>
2015-07-21 16:52:38 -0700

    fixed formatting for left side menu with buttons now

--- DBproject.py
+++ DBproject.py
@@ -20,27 +20,53 @@ def exit_loop(key):
 
 def main():
   text_header = (u" Welcome to our CS419 project! q or Q exits the program.")
-  text_leftcol1 = (u"Top of left column")
-  text_leftcol2 = (u"This is the body of the left column")
+  text_leftcol1_1 = (u"Database:")
+  text_leftcol1_2 = (u"Test DB")
+  text_leftcol2_1 = (u"Tables:")
+  text_leftcol2_2 = (u"Table 1")
+  text_leftcol2_3 = (u"Table 2")
+  text_leftcol2_4 = (u"Table 3")
+  text_leftcol2_5 = (u"Table 4")
+  text_leftcol2_6 = (u"Table 5")
+  text_leftcol2_7 = (u"Table 6")
   text_maintop = (u"This is the top section of the main body")
   text_mainbody = (u" This is the main body section")
   text_instructions = (u"This program allows you to connect to a PostgreSQL or MySQL database and then perform operations on that databse. The program is written in python and is an ncurses based command line tool.")
   
+  #this creates blank as a simple divider widget to add a line of space
   blank = urwid.Divider()
+
+  #Signal handler for the left side buttons
+  def leftcol_btn_press(button):
+      frame.footer = urwid.AttrWrap(urwid.Text(
+          [u" Pressed: ", button.get_label()]), 'header')
+
+  #creating left column widget
+  left_column = urwid.AttrWrap( urwid.Padding (urwid.Pile([
+          blank,
+          urwid.Text(text_leftcol1_1, align='center'),
+          urwid.AttrWrap( urwid.Button(text_leftcol1_2, leftcol_btn_press), 'btn', 'btnf'),
+          urwid.Divider("="),
+          blank,
+          urwid.Text(text_leftcol2_1, align='center'),
+          urwid.AttrWrap( urwid.Button(text_leftcol2_2, leftcol_btn_press), 'btn', 'btnf'),
+          urwid.AttrWrap( urwid.Button(text_leftcol2_3, leftcol_btn_press), 'btn', 'btnf'),
+          urwid.AttrWrap( urwid.Button(text_leftcol2_4, leftcol_btn_press), 'btn', 'btnf'),
+          urwid.AttrWrap( urwid.Button(text_leftcol2_5, leftcol_btn_press), 'btn', 'btnf'),
+          urwid.AttrWrap( urwid.Button(text_leftcol2_6, leftcol_btn_press), 'btn', 'btnf'),
+          urwid.AttrWrap( urwid.Button(text_leftcol2_7, leftcol_btn_press), 'btn', 'btnf'),
+          blank
+        ])
+      , left=1, right=2)
+    , 'leftside')
+
+
   listbox_content = [
     blank,
     urwid.Padding(urwid.Text(text_instructions), left=2, right=2),
     blank,
     urwid.Columns([
-      ('fixed', 15, 
-        urwid.AttrWrap( urwid.Padding( urwid.Pile([
-              urwid.Text(text_leftcol1),
-              urwid.Divider("~"),
-              urwid.Text(text_leftcol2)
-            ])
-          , left=1, right=1)
-        , 'leftside')
-      ),
+      ('fixed', 17, left_column),
       urwid.AttrWrap( urwid.Padding( urwid.Pile([
             urwid.AttrWrap( urwid.Text(text_maintop), 'topmenu'),
             urwid.AttrWrap( urwid.Divider("-"), 'topmenu'),
@@ -73,9 +99,11 @@ def main():
   palette = [
       ('header', 'light gray', 'dark red'),
       ('topmenu', 'light gray', 'dark blue'),
-      ('leftside', 'black', 'dark cyan'),
-      ('body', '', 'black'),
-      ('bg', 'black', 'light gray')
+      ('leftside', 'light gray', 'dark cyan'),
+      ('body', 'black', 'light gray'),
+      ('bg', 'black', 'light gray'),
+      ('btn','light gray','dark cyan'),
+      ('btnf','light gray','dark blue')
   ]
 
   loop = urwid.MainLoop(frame, palette, unhandled_input=exit_loop, screen=urwid.curses_display.Screen())
