0a29e3c88150018830dcc8592645552719b00c05
Adam Pedigo <pedigoa@onid.oregonstate.edu>
2015-08-05 16:47:34 -0400

    implemented msyql up to the same functionality as psql

--- mysqlDB.py
+++ mysqlDB.py
@@ -25,9 +25,9 @@ def connectdb(name, user, pw):
 # returns list of table names on success, -1 on failure
 def gettables(cur):
   try:
-    sql ="""SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 
+    SQL ="""SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 
            'BASE TABLE' AND TABLE_SCHEMA = '""" + cur.ourdbname +  """';"""
-    cur.execute(sql)
+    cur.execute(SQL)
     data = cur.fetchall()
     #print data
     names = []
@@ -35,4 +35,32 @@ def gettables(cur):
       names.append(table[0])
     return names
   except:
+    return -1
+
+# takes cursor object
+# returns list of row data on success, -1 on failure
+def allrows(cur, name):
+  SQL = "SELECT * FROM " + name + ";"
+  try:
+    cur.execute(SQL)
+    data = cur.fetchall()
+    rows = []
+    for row in data:
+      rows.append(row)
+    return rows
+  except:
+    return -1
+
+# takes cursor object
+# returns list of all column names on success, -1 on failure
+def getcolnames(cur, table):
+#  try:
+    SQL = """SELECT DISTINCT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='""" + table + """';"""
+    cur.execute(SQL)
+    data = cur.fetchall()
+    columns = []
+    for column in data:
+      columns.append(column[0])
+    return columns
+#  except:
     return -1
