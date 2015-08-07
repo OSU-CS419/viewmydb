83fd6328e7847c816e8175a6631f708fe1152b92
davidtadams <adams.t.david@gmail.com>
2015-08-07 17:53:34 -0500

    separate files for table browse and structure

---
+++ TableBrowse.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import urwid
+
+"""
+NOTES
+-----
+This module dynamically builds a table to show all the rows in a
+table including column names.
+
+It is based on David's DBstructure.py
+
+Currently breaks when there are no rows to render
+
+"""
+
+# reorganizes row list for a column-oriented view, e.g.
+# returns a list of widget lists
+def splitTable(allrows):
+  cols = []
+  if allrows:
+    for i in range(0, len(allrows[0])):
+      col = []
+      for index, row in enumerate(allrows):
+        col.append(urwid.Text(str(row[i])))
+      cols.append(col)
+
+  return cols
+
+# creates a widget consisting of a table of all data in the rows
+# with column headers
+# takes a list of column names and a list of (list)rows
+def showTables(colnames, rowdata):
+  widget_lists = splitTable(rowdata)		# get a list of a list of widgets
+  columns = []					# empty columns list
+  for i in range (0, len(colnames)):		# for each column
+    if widget_lists:				# if list not empty
+      mypile = urwid.Pile(widget_lists[i])	# make a Pile with the list of widgets
+    else:
+      mypile = urwid.Pile([			# a blank widget to fill up some space
+      urwid.Text(u""),
+    ])
+    # make a linebox with the Pile and the columnname
+    mylinebox = (urwid.LineBox((mypile), title=colnames[i], rline=' ', trcorner=u'\u2500', brcorner=u'\u2500'))
+    columns.append(mylinebox)			# append the linebox to the list of columns
+
+  #signal handler for the more button
+  def more_btn_press(button):
+    print "this is the more button"
+
+  #more button to show more results
+  #only show if results are greater than a certain amount
+  more_btn = urwid.AttrWrap( urwid.Button(u"More", more_btn_press), 'btnf', 'btn')
+  more_btn = urwid.Padding(more_btn, width=8)
+
+  structure_view = urwid.Padding(urwid.Pile([urwid.Columns(columns),more_btn]), left=2, right=2)
+  structure_view = urwid.WidgetPlaceholder(structure_view)
+
+  #return the widget created that holds all the structure data
+  return structure_view
--- TableStructure.py
+++ TableStructure.py
@@ -5,64 +5,19 @@ import urwid
 """
 NOTES
 -----
-This module builds the widget to show the structure information for a database.
+This module builds the widget to show the structure information for a table.
 
 """
 
-def show_db_structure(user_info):
-  #build out the table that shows the db structure stats
-  #get db info
-  db_info = user_info.db_obj.getdbinfo(user_info.db_conn, user_info.db_name)
+def show_table_structure(user_info):
+  #build out table to show table stats
 
-  #get table names and size
-  db_tableinfo = user_info.db_obj.getdb_tableinfo(user_info.db_conn)
-
-  #building out list of table name text widgets
-  table_names_list = []
-  for x in db_tableinfo:
-    table_names_list.append(urwid.Text(x[0]))
+  #show attribute information
+  #field
+  #type
+  #attributes
+  #null
+  #default
+  #extra
+  #...like phpadmin...
   
-  #building out list of table size text widgets
-  table_sizes_list = []
-  for x in db_tableinfo:
-    table_sizes_list.append(urwid.Text(x[1]))
-
-
-  #widgets for the database info table
-  db_name_col = urwid.LineBox( 
-      urwid.Text(db_info[0])
-    , title="DB Name", rline=' ', trcorner=u'\u2500', brcorner=u'\u2500')
-  encoding_col = urwid.LineBox( 
-      urwid.Text(db_info[1])
-    , title="Encoding", rline=' ', trcorner=u'\u2500', brcorner=u'\u2500')
-  collation_col = urwid.LineBox( 
-      urwid.Text(db_info[2])
-    , title="Encoding")
-
-  #widgets for the database tables info table
-  table_names = urwid.LineBox( urwid.Pile(table_names_list)
-    , title="Name", rline=' ', trcorner=u'\u2500', brcorner=u'\u2500')
-  table_sizes = urwid.LineBox( urwid.Pile(table_sizes_list)
-    , title="Size")
-
-  #main widget for view
-  structure_view = urwid.Padding( urwid.Pile( [
-    urwid.Text(u"Database Info:"),
-    urwid.Columns([
-        db_name_col,
-        encoding_col,
-        collation_col
-      ]),
-    urwid.Divider(),
-    urwid.Text(u"DB Tables:"),
-    urwid.Columns([
-        table_names,
-        table_sizes
-      ])
-    ])
-  , left=2, right=2)
-
-  structure_view = urwid.WidgetPlaceholder(structure_view)
-
-  #return the widget created that holds all the structure data
-  return structure_view
