9071855c22bc41218ac7ede24fbea304c32aa24a
davidtadams <adams.t.david@gmail.com>
2015-08-13 19:16:33 -0500

    final version at submission

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
   name='viewmydb',
-  version='0.1.4',
+  version='0.1.5',
   description='Python terminal GUI for MySQL and PostgreSQL DBs.',
   classifiers=[
     'Development Status :: 4 - Beta',
